3b7f74669a2d3619a90024069ddcc52c61773ebb
Phuong Vo <vuphu.itus@gmail.com>
2024-07-07 17:59:14 +0700

    feat: update code base

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,4 +1,18 @@
-import OrmConfig from './orm.config';
+import { env } from './env.config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
-export const DbConfig = TypeOrmModule.forRoot(OrmConfig);
+export const DbConfig = TypeOrmModule.forRoot(<PostgresConnectionOptions>{
+  type: 'postgres',
+  host: env.DATABASE_HOST,
+  port: env.DATABASE_PORT,
+  username: env.DATABASE_USER,
+  password: env.DATABASE_PASSWORD,
+  database: env.DATABASE_NAME,
+  synchronize: false,
+  entities: [`dist/modules/*/models/*.js`],
+  migrations: [`dist/modules/*/migrations/*.js`],
+  migrationsRun: true,
+  namingStrategy: new SnakeNamingStrategy(),
+});
