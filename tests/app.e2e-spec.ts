8e6769096bd623a6762d07cdeba495a21beef6eb
vuphu <vuphu.itus@gmail.com>
2025-01-17 20:09:07 +0700

    ci: add e2e test workflow

--- tests/app.e2e-spec.ts
+++ tests/app.e2e-spec.ts
@@ -1,12 +1,12 @@
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
